00768aa93724be90c2a583d76ded8f5dc6d401d7
Jack Drabenstadt <jackdrabenstadt@gmail.com>
2026-04-02 16:27:12 -0400

    a

--- libraries/libraries/slskd-betanin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-FILE=$(echo "$SLSKD_SCRIPT_DATA" | grep -o '"localFilename":"[^"]*"' | cut -d'"' -f4)
-DIR=$(dirname "$FILE")
-NAME=$(basename "$DIR")
-wget -q -O/dev/null \
-  --post-data "path=/downloads&name=$NAME" \
-  --header "X-API-Key: dfe127c81fab58fe3aaa948de167bedd" \
-  http://betanin:9393/api/torrents
